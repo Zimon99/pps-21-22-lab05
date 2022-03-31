dd7044e077487f4325e37047eb40cc84b049ee9f
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-03-31 20:11:47 +0200

    Es1 - 1 Test

--- src/test/scala/u05lab/ex1/ListTest.scala
+++ src/test/scala/u05lab/ex1/ListTest.scala
@@ -1,10 +1,15 @@
 package u05lab.ex1
 
 import org.junit.Test
+import org.junit.Assert.*
 
 class ListTest {
 
-  @Test
-  def testZipRight(): Unit = {}
+  private var list = List(1, 2, 3, 4)
+  private var zipList = List((1, 0), (2, 1), (3, 2), (4, 3))
+
+  @Test def testZipRightWithRec(): Unit = {
+    assertEquals(zipList, list.zipRightWithRec)
+  }
 
 }
